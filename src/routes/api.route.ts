dd11a335bd76b787f925b90e8dc0eec700805ab2
cakrapand <cakrapand@gmail.com>
2024-06-03 21:06:11 +0700

    docs: swagger api docs

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { carsRouter } from "./cars.route";
 import { authRouter } from "./auth.route";
 import { usersRouter } from "./users.route";
-import { authorize } from "../middleware/authorize";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "../docs/swagger";
 
 export const router = Router();
 
 router.use("/api", authRouter);
 router.use("/api", usersRouter);
 router.use("/api", carsRouter);
+router.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
